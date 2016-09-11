480f56e78ff0030e5c89c87161ad85d427c331d5
Nikita Trophimov <b0r3d0mness@gmail.com>
2016-09-11 17:30:43 +0400

    Add config tests, add Travis CI support

--- test/clojukh/core_test.clj
+++ test/clojukh/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [clojukh.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest config-test
+  (testing "contains all the required fields"
+    (is (every? #(get-in config %) [[:general :port]
+                                    [:general :keystore]
+                                    [:general :key-password]
+                                    [:general :crashrpts-dir]
+                                    [:email :smtp]
+                                    [:email :from]
+                                    [:email :pass]
+                                    [:email :to]]))))
